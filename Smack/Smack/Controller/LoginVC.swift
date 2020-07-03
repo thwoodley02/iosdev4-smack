987d9c9ff975ffedcceecbd82a1cea7027e446ab
Tom <thwoodley@gmail.com>
2020-07-03 13:48:13 +0100

    End of Session 4

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Tom Woodley on 03/07/2020.
+//  Copyright © 2020 Tom Woodley. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //IB Actions
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Tom Woodley on 02/07/2020.
+//  Copyright © 2020 Tom Woodley. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
